ddd7e22d5fe42fb47806837025bdf609cbccc86d
Thabiso <leeuwthabiso3@gmail.com>
2026-09-10 13:18:38 +0200

    Task1

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 
-let greeting = "hello"
+//Input
+const inventory = {
+    items: [
+        {
+            name: 'Laptop',
+            price: 999.99,
+            quantity: 5
+        },
+        {
+            name: 'Mouse',
+            price: 25,
+            quantity: 5
+        },
+        {
+            name: 'Keyboard',
+            price: 45.50,
+            quantity: 0
+        },
+        {
+            name: 'Monitor',
+            price: 150,
+            quantity: 8
+        }
+    ]
+}
+console.log(inventory)
 
-console.log(greeting)
+//Proccessing
+function Update(){
+    items[2].quantity = 20;
+}
+console.log(inventory)
+
+//Output
+function TotalVal(totalInventoryValue) {
+  totalInventoryValue = inventory.items.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+}
+console.log(totalInventoryValue);
